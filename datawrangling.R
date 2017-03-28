ef21495515afe7400122385eb1c614e700c48124
Þórgnýr Thoroddsen <thorgnyr@lanti.cc>
2017-03-28 13:32:47 +0000

    data input

---
+++ datawrangling.R
@@ -0,0 +1,4 @@
+##Wrangle the data so that there are no text issues.
+housing_prices <- read.table("data/fasteignir.csv", header = TRUE, sep = ";", dec = ".")
+
+##Create reference sheet for Vianny.
